6afa639f0c133e827b6e079fa43c0773240d103b
jcopado <jcopado@surlabs.es>
2023-08-12 23:39:38 +0200

    style (XII)

--- templates/tpl.il_as_qpl_xqscala_output.js
+++ templates/tpl.il_as_qpl_xqscala_output.js
@@ -1,11 +1,11 @@
-// JavaScript for interactivity
+// JavaScript para interactividad
 document.querySelectorAll('.scala-table input[type="radio"]').forEach(radio => {
     radio.addEventListener('change', (event) => {
-        // remove existing highlighted rows
+        // Remove any existing highlighted rows
         document.querySelectorAll('.scala-table tr').forEach(row => {
-            row.style.backgroundColor = "#fff";
+            row.style.backgroundColor = "transparent";
         });
-        // highlight the selected row
-        event.target.closest('tr').style.backgroundColor = "#d8e1e8";
+        // Highlight the selected row
+        event.target.closest('tr').style.backgroundColor = "rgba(76, 101, 134, 0.1)";
     });
 });
